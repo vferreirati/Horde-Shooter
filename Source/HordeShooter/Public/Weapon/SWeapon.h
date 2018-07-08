226b16e7fb3997fd54b7c2b673be6a906dd2a2de
Victor Ferreira <vferreirati@gmail.com>
2018-07-08 16:34:00 -0300

    Create Weapon Class

---
+++ Source/HordeShooter/Public/Weapon/SWeapon.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SWeapon.generated.h"
+
+UCLASS()
+class HORDESHOOTER_API ASWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASWeapon();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class USkeletalMeshComponent* MeshComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};

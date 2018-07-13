7027cfa06525de3bd82d67f38232d0ac34c37170
Victor Ferreira <vferreirati@gmail.com>
2018-07-13 12:29:42 -0300

    SuperSpeed powerup bugfix and material

--- Source/HordeShooter/Private/Powerups/SPowerupActor.cpp
+++ Source/HordeShooter/Private/Powerups/SPowerupActor.cpp
@@ -31,10 +31,11 @@ void ASPowerupActor::OnTickPowerup() {
 
 void ASPowerupActor::ActivatePowerup() {
 	
+	OnActivated();
+
 	if (PowerupInterval > 0.f) {
-		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true, 0.f);
-	}
-	else {
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true);
+	} else {
 		OnTickPowerup();
 	}
 }

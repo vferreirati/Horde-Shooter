4f12ae1593a0f3c27834aedad8a088006b89980b
Victor Ferreira <vferreirati@gmail.com>
2018-07-14 14:07:55 -0300

    Wave based spawning

--- Source/HordeShooter/Private/SGameMode.cpp
+++ Source/HordeShooter/Private/SGameMode.cpp
@@ -1,7 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SGameMode.h"
+#include "TimerManager.h"
 
+ASGameMode::ASGameMode() {
+	TimeBetweenWaves = 2.f;
+}
 
+void ASGameMode::StartPlay() {
+	Super::StartPlay();
 
+	PrepareForNextWave();
+}
 
+void ASGameMode::StartWave() {
+	++WaveCount;
+	NumberOfBotsToSpawn = 2 * WaveCount;
+
+	// Sets timer to spawn bots
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.f, true, 0.f);
+}
+
+void ASGameMode::EndWave() {
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
+
+void ASGameMode::SpawnBotTimerElapsed() {
+	SpawnNewBot();
+
+	--NumberOfBotsToSpawn;
+
+	if (NumberOfBotsToSpawn <= 0) {
+		EndWave();
+	}
+}
+
+void ASGameMode::PrepareForNextWave() {
+	FTimerHandle TimerHandle_NextWaveStart;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
--- Source/HordeShooter/Public/SGameMode.h
+++ Source/HordeShooter/Public/SGameMode.h
@@ -13,8 +13,41 @@ UCLASS()
 class HORDESHOOTER_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
 	
+protected:
+	// Timer that deals with bot spawning logic
+	FTimerHandle TimerHandle_BotSpawner;
+
+	// Bots to spawn in current wave.
+	int32 NumberOfBotsToSpawn;
+
+	// Current wave number
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gamemode")
+	float TimeBetweenWaves;
+
+protected:
+
+	// Spawns a bot in the level
+	// Implemented in blueprint
+	UFUNCTION(BlueprintImplementableEvent, Category = "Gamemode")
+	void SpawnNewBot();
+
+	// Verifies if there's a bot slot in the current wave
+	// Spawns only if true.
+	void SpawnBotTimerElapsed();
 	
-	
-	
+	// Start spawning bots
+	void StartWave();
+
+	// Stop spawning bots
+	void EndWave();
+
+	// Sets timer for next wave 
+	void PrepareForNextWave();
 };
